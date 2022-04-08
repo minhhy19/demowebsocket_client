170dacafed3c946d45dc27ff070a267bfe2342cd
Pham Minh Hy <hypm@MECORP.LOCAL>
2022-04-08 17:23:26 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WebSocketDemo from './components/WebSockerDemo.jsx/WebSocketDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <WebSocketDemo />
+    </div>
+  );
+}
+
+export default App;
